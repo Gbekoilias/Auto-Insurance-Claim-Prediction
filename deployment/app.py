79831dbd32d1dec926a444bc683266199a938f16
gbekoilias <gbekoilias@gmail.com>
2023-12-03 16:32:15 +0100

    added a cover img to the web app

--- deployment/app.py
+++ deployment/app.py
@@ -10,15 +10,29 @@ from sklearn.preprocessing import OrdinalEncoder
 from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import OneHotEncoder
 
+def load_model():
+    with open('C:/Users/DONKAMS/Downloads/Project_STA2017/saved_steps.pkl','rb') as file:
+        data=pickle.load(file)
+    return data
+data=load_model()
+classifier=data['model']
+le_gender=data['le_gender']
+le_category=data['le_category']
+le_make=data['le_make']
+le_product=data['le_product']
+le_colour=data['le_colour']
+le_state=data['le_state']
+le_lga=data['le_lga']
+
 # Initialize transformers
 LE = LabelEncoder()
 SCL = StandardScaler()
 OE = OneHotEncoder()
-
+#background-image: url("https://github.com/Gbekoilias/Project_STA2017/blob/main/deployment/auto%20cover.jpg?raw=true") no-repeat center center fixed;
 st.markdown("""
     <style>
     .main {
-        background: url("https://github.com/Gbekoilias/Project_STA2017/blob/main/deployment/auto%20cover.jpg?raw=true") no-repeat center center fixed; 
+         background-image: url("https://www.investopedia.com/thmb/NUc9wn8E6cMh2lbVmaBFwSNuETM=/2121x1414/filters:fill(auto,1)/GettyImages-1186091457-0c53b6749ceb49b79053f61f583eef03.jpg");
         -webkit-background-size: cover;
         -moz-background-size: cover;
         -o-background-size: cover;
@@ -26,10 +40,6 @@ st.markdown("""
     }
     </style>
     """, unsafe_allow_html=True)
-
-#load model using pickle
-model_file_path='C:/Users/DONKAMS/Downloads/Project_STA2017/PIPEmodelGBC.sav'
-model = pickle.load(open(model_file_path, 'rb'))
 
 #main function
 def show_prediction():
@@ -95,37 +105,28 @@ def show_prediction():
     State=st.selectbox("State where policy was purchased?",State)
     ProductName=st.selectbox("Name of the car product?",ProductName)
     ok=st.button("Check if it's a claim or not")
-    from sklearn.preprocessing import LabelEncoder
-
-    # Initialize LabelEncoder
-    LE = LabelEncoder()
-
     if ok:
-        # Convert categorical variables to numeric
-        Gender = LE.fit_transform([Gender])[0]
-        Car_Category = LE.fit_transform([Car_Category])[0]
-        Subject_Car_Colour = LE.fit_transform([Subject_Car_Colour])[0]
-        Subject_Car_Make = LE.fit_transform([Subject_Car_Make])[0]
-        LGA_Name = LE.fit_transform([LGA_Name])[0]
-        State = LE.fit_transform([State])[0]
-        ProductName = LE.fit_transform([ProductName])[0]
+        X=np.array([[Gender,Age,No_Pol,Car_Category,Subject_Car_Colour,Subject_Car_Make,LGA_Name,State,ProductName]])
+        X[:,0]=le_gender.fit_transform(X[:,0])
+        X[:,3]=le_category.fit_transform(X[:,3])
+        X[:,4]=le_colour.fit_transform(X[:,4])
+        X[:,5]=le_make.fit_transform(X[:,5])
+        X[:,6]=le_lga.fit_transform(X[:,6])
+        X[:,7]=le_state.fit_transform(X[:,7])
+        X[:,8]=le_product.fit_transform(X[:,8])
 
-        # Scale numeric variables
-        Age = SCL.fit_transform([[Age]])[0][0]
-        No_Pol = SCL.fit_transform([[No_Pol]])[0][0]
+        X=X.astype(float)
 
-        # Create input_data array
-        input_data = np.array([[Gender, Age, No_Pol, Car_Category, Subject_Car_Colour, Subject_Car_Make, LGA_Name, State, ProductName]])
+        claim=classifier.predict(X)
+        #st.subheader(f"T {claim}")
 
-        # Convert input_data to a pandas DataFrame
-        input_data = pd.DataFrame(input_data, columns=['Gender', 'Age', 'No_Pol', 'Car_Category', 'Subject_Car_Colour', 'Subject_Car_Make', 'LGA_Name', 'State', 'ProductName'])
-
-        claim = model.predict(input_data)
-        prediction = model.predict_proba(input_data)[:, 1]
+        prediction=classifier.predict_proba(X)[:, 1]
+        #st.subheader(f"T {prediction}")
         if prediction >= 0.16: # 'spam':
             st.subheader("The client will renew its claim")
         else:
-                st.subheader("The client will not renew its claim")
+            st.subheader("The client will not renew its claim")
+        #st.subheader(f"{prediction}")
 show_prediction()
 # Make predictions
 #prediction = model.predict(input_data)  # Replace with your actual prediction logic
